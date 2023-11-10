0ca99bea46e59aeb3dc3cfc7a4e9ee25bac125fc
IttaYoshi <ittalian0329@gmail.com>
2023-11-11 02:41:34 +0900

    日記削除機能追加

--- diary/views.py
+++ diary/views.py
@@ -74,3 +74,15 @@ class DiaryUpdateView(LoginRequiredMixin, generic.UpdateView):
         messages.error(self.request, '日記の更新に失敗しました。')
         return super().form_invalid(form)
     
+class DiaryDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Diary
+    template_name = 'diary_delete.html'
+    success_url = reverse_lazy('diary_list')
+
+    def form_valid(self, form):
+        messages.success(self.request, "日記を削除しました。")
+        return super().form_valid(form)
+
+    def delete(self, request, *args, **kwargs):
+        # messages.success(self.request, '日記を削除しました。')
+        return super().delete(request, *args, **kwargs)
